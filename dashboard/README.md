10d210fd78163f8ff98fbe13c1e4611398737d40
solag <guillem.sola@dowjones.com>
2018-12-28 01:23:10 +0100

    Dashboard

---
+++ dashboard/README.md
@@ -0,0 +1,30 @@
+# vue-test
+
+> A project using bootstrap Vue.js
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
+
+For detailed explanation on how things work, consult the [docs for vue-loader](http://vuejs.github.io/vue-loader).
+
+## Mobile screenshots
+
+### Iphone 6
+750*1334 - 0.5622188905547226
+wiki frame 410*835
+wiki hole 361*636 - 0.5676100628930818
+0,4813*0,76167
+
+## Style
+
+https://bootswatch.com/slate/
---
+++ dashboard/src/App.vue
@@ -0,0 +1,107 @@
+<template>
+  <div id="app">
+
+    <div class="navbar navbar-expand-lg fixed-top navbar-dark bg-primary" style="">
+      <div class="container">
+        <a href="../" class="navbar-brand">{{ title }}</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link" href="../help/">Help</a>
+            </li>
+          </ul>
+
+          <ul class="nav navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="https://wrapbootstrap.com/?ref=bsw" target="_blank">WrapBootstrap</a>
+            </li>
+          </ul>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="bs-docs-section clearfix">
+        <div class="row">
+          <div class="input-group mb-3">
+          <div class="input-group-prepend">
+            <span class="input-group-text" id="inputGroup-sizing-default">Website URL</span>
+          </div>
+          <input type="text" class="form-control" aria-label="Website URL" aria-describedby="inputGroup-sizing-default" :value="websiteURL" @change="refreshUrl" />
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-6 col-sm-12">
+            <div class="card mt-2">
+              <div class="card-header card-outline-success">
+                <span>iPhone 6</span>
+              </div>
+              <div class="card-body">
+                <img class="mobile" src="./assets/IPhone6_silver_frontface.png" />
+                <img class="screenshot" :src="encodedURL" width="363">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      websiteURL: 'https://www.google.com',
+      title: 'Screenshots Dashboard'
+    }
+  },
+  computed: {
+    encodedURL: function () {
+      return 'http://localhost:3000/api/screenshot?url=' + encodeURIComponent(this.websiteURL) + '&device=iPhone%206';
+    }
+  },
+  methods : {
+    refreshUrl: function (e) {
+      console.log(this.websiteURL);
+      this.websiteURL = e.target.value
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
