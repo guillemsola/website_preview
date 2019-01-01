4732fd893bb9340e6a5f9b4b6ccdaa1d5c62ecdd
solag <guillem.sola@dowjones.com>
2019-01-01 23:48:24 +0100

    Add serving files from public folder to later on add dashboard

--- api/server.js
+++ api/server.js
@@ -5,6 +5,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 require('./app/routes')(app, {});
+app.use(express.static('public'))
 
 app.listen(port, () => {
     console.log('We are live on ' + port);
